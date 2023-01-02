193dde1f605f94a17ef64c467159bd2694961720
Mohamedzamra <mohamedzamra76@gmail.com>
2023-01-02 14:57:14 +0200

    first commit

---
+++ resources/views/layouts/footer_script.blade.php
@@ -0,0 +1,12 @@
+<script src={{asset("assets/js/jquery-3.3.1.min.js")}}></script>
+<!-- https://jquery.com/download/ -->
+<script src={{asset("assets/js/moment.min.js")}}></script>
+<!-- https://momentjs.com/ -->
+<script src={{asset("assets/js/utils.js")}}></script>
+<script src="js/Chart.min.js"></script>
+<!-- http://www.chartjs.org/docs/latest/ -->
+<script src={{asset("assets/js/fullcalendar.min.js")}}></script>
+<!-- https://fullcalendar.io/ -->
+<script src={{asset("assets/js/bootstrap.min.js")}}></script>
+<!-- https://getbootstrap.com/ -->
+<script src={{asset("assets/js/tooplate-scripts.js")}}></script>
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,13 @@
+<title>Dashboard Admin Template by Tooplate.com</title>
+
+
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600">
+<!-- https://fonts.google.com/specimen/Open+Sans -->
+<link rel="stylesheet" href={{asset("assets/css/fontawesome.min.css")}}>
+<!-- https://fontawesome.com/ -->
+<link rel="stylesheet" href={{asset("assets/css/fullcalendar.min.css")}}>
+<!-- https://fullcalendar.io/ -->
+<link rel="stylesheet" href={{asset("assets/css/bootstrap.min.css")}}>
+<!-- https://getbootstrap.com/ -->
+<link rel="stylesheet" href={{asset("assets/css/tooplate.css")}}>
